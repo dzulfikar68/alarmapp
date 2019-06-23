5ffb441adb504d0615d4f4d89cb8ec89c1a31bc4
dzulfikar68 <dzulfikar.dsdevs@gmail.com>
2019-06-23 15:14:07 +0700

    last commit

--- app/src/main/java/com/digitcreativestudio/alarmapp/MainActivity.java
+++ app/src/main/java/com/digitcreativestudio/alarmapp/MainActivity.java
@@ -7,41 +7,73 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.LayoutDirection;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     TimePicker myTimePicker;
     Button buttonstartSetDialog;
-    TextView textAlarmPrompt;
+//    TextView textAlarmPrompt;
 
     TimePickerDialog timePickerDialog;
 
     final static int RQS_1 = 1;
+
+    RecyclerView rv_alarm;
+    ArrayList<Model> dataSet;
+    AlarmAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textAlarmPrompt = (TextView) findViewById(R.id.alarmprompt);
+//        textAlarmPrompt = (TextView) findViewById(R.id.alarmprompt);
+
+        dataSet = new ArrayList<>();
+        rv_alarm = findViewById(R.id.rv_alarm);
+        rv_alarm.setHasFixedSize(true);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        rv_alarm.setLayoutManager(layoutManager);
+
+        adapter = new AlarmAdapter(dataSet);
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        adapter.setInitial(getBaseContext(), alarmManager);
+        rv_alarm.setAdapter(adapter);
 
         buttonstartSetDialog = (Button) findViewById(R.id.startSetDialog);
         buttonstartSetDialog.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                textAlarmPrompt.setText("");
+//                textAlarmPrompt.setText("");
                 openTimePickerDialog(false);
 
             }
         });
+    }
+
+    private void openTimePickerDialog(boolean is24r) {
+        Calendar calendar = Calendar.getInstance();
+
+        timePickerDialog = new TimePickerDialog(MainActivity.this,
+                onTimeSetListener, calendar.get(Calendar.HOUR_OF_DAY),
+                calendar.get(Calendar.MINUTE), true);
+        timePickerDialog.setTitle("Set Alarm Time");
+
+        timePickerDialog.show();
+
     }
 
     TimePickerDialog.OnTimeSetListener onTimeSetListener = new TimePickerDialog.OnTimeSetListener() {
@@ -57,6 +89,7 @@ public class MainActivity extends AppCompatActivity {
             calSet.set(Calendar.SECOND, 0);
             calSet.set(Calendar.MILLISECOND, 0);
 
+            //pengecekan jika waktu sekarang ....
             if (calSet.compareTo(calNow) <= 0) {
                 // Today Set time passed, count to tomorrow
                 calSet.add(Calendar.DATE, 1);
@@ -73,28 +106,19 @@ public class MainActivity extends AppCompatActivity {
 
     private void setAlarm(Calendar targetCal) {
 
-        textAlarmPrompt.setText("***\n" + "Alarm set on " + targetCal.getTime()
-                + "\n***");
+//        String info = "***\n" + "Alarm set on " + targetCal.getTime() + "\n***";
+//        textAlarmPrompt.setText(info);
+
+        int key = (int)(Math.random() * 50 + 1);
 
         Intent intent = new Intent(getBaseContext(), AlarmReceiver.class);
         PendingIntent pendingIntent = PendingIntent.getBroadcast(
-                getBaseContext(), RQS_1, intent, 0);
+                getBaseContext(), RQS_1, intent, key);
         AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
         alarmManager.set(AlarmManager.RTC_WAKEUP, targetCal.getTimeInMillis(),
                 pendingIntent);
 
-    }
-
-
-    private void openTimePickerDialog(boolean is24r) {
-        Calendar calendar = Calendar.getInstance();
-
-        timePickerDialog = new TimePickerDialog(MainActivity.this,
-                onTimeSetListener, calendar.get(Calendar.HOUR_OF_DAY),
-                calendar.get(Calendar.MINUTE), true);
-        timePickerDialog.setTitle("Set Alarm Time");
-
-        timePickerDialog.show();
-
+        dataSet.add(new Model(key, targetCal.getTime().toString()));
+        adapter.notifyDataSetChanged();
     }
 }
